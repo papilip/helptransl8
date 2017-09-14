4c2ba62ddb482d40fbabb80bd499aaee3d9dd4f5
papilip <me@papilip.me>
2017-09-14 15:34:18 +0200

    crystal init app helptransl8

---
+++ spec/helptransl8_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Helptransl8 do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/helptransl8.cr
@@ -0,0 +1,5 @@
+require "./helptransl8/*"
+
+module Helptransl8
+  # TODO Put your code here
+end
