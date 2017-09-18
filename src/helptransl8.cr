1bf030a3779ac894235e9754d5905d5213e093bd
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-09-18 20:04:33 +0300

    Format sources with `crystal tool format`

--- src/helptransl8.cr
+++ src/helptransl8.cr
@@ -18,38 +18,34 @@ OptionParser.parse! do |parser|
     # puts parser
   end
 
-  parser.on(short_flag:  "-c"
-           ,long_flag:   "--check"
-           ,description: "Check each file with the original repository"
-           ) do
+  parser.on(
+    short_flag: "-c",
+    long_flag: "--check",
+    description: "Check each file with the original repository") do
     Helptransl8::Check.new.run
   end
 
-  parser.on(short_flag:  "-h"
-           ,long_flag:   "--help"
-           ,description: "Show this help"
-           ) do
+  parser.on(short_flag: "-h",
+    long_flag: "--help",
+    description: "Show this help") do
     puts parser
   end
 
-  parser.on(short_flag:  "-i URL"
-           ,long_flag:   "--init=URL"
-           ,description: "Generates the helptransl8.yml file with original repository url"
-           ) do |url|
+  parser.on(short_flag: "-i URL",
+    long_flag: "--init=URL",
+    description: "Generates the helptransl8.yml file with original repository url") do |url|
     Helptransl8::Config.new.init url
   end
 
-  parser.on(short_flag:  "-p"
-           ,long_flag:   "--print"
-           ,description: "Print URL from original repository"
-           ) do
+  parser.on(short_flag: "-p",
+    long_flag: "--print",
+    description: "Print URL from original repository") do
     Helptransl8::Config.new.print
   end
 
-  parser.on(short_flag:  "-v"
-           ,long_flag:   "--version"
-           ,description: "Shows the version"
-           ) do
+  parser.on(short_flag: "-v",
+    long_flag: "--version",
+    description: "Shows the version") do
     puts "Helptransl8 (#{Helptransl8::VERSION})"
   end
 end
